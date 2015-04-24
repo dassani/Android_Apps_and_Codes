715b05172cfeff17b5ee5a2681e4a6f2390255f6
Vipul Dassani <dassani.vipul@gmail.com>
2015-04-24 15:47:44 +0530

    Google Maps demo
    This project has many Map examples. Simplifies the understanding of how
    Google Maps v2 works.

---
+++ GoogleMapsDemo/src/com/example/mapdemo/MyLocationDemoActivity.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.mapdemo;
+
+import java.util.List;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This demo shows how GMS Location can be used to check for changes to the
+ * users location. The "My Location" button uses GMS Location to set the blue
+ * dot representing the users location. To track changes to the users location
+ * on the map, we request updates from the {@link LocationClient}.
+ */
+public class MyLocationDemoActivity extends FragmentActivity implements
+		ConnectionCallbacks, OnConnectionFailedListener,
+		OnMyLocationButtonClickListener, LocationListener {
+
+	private GoogleMap mMap;
+
+	private LocationClient mLocationClient;
+	private TextView mMessageView;
+
+	// These settings are the same as the settings for the map. They will in
+	// fact give you updates
+	// at the maximal rates currently possible.
+	private static final LocationRequest REQUEST = LocationRequest.create()
+			.setInterval(5000) // 5 seconds
+			.setFastestInterval(16) // 16ms = 60fps
+			.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.my_location_demo);
+		mMessageView = (TextView) findViewById(R.id.message_text);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+		setUpLocationClientIfNeeded();
+		mLocationClient.connect();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mLocationClient != null) {
+			mLocationClient.disconnect();
+		}
+	}
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				mMap.setMyLocationEnabled(true);
+				mMap.setOnMyLocationButtonClickListener(this);
+			}
+		}
+	}
+
+	private void setUpLocationClientIfNeeded() {
+		if (mLocationClient == null) {
+			mLocationClient = new LocationClient(getApplicationContext(), this, // ConnectionCallbacks
+					this); // OnConnectionFailedListener
+		}
+	}
+
+	/**
+	 * Button to get current Location. This demonstrates how to get the current
+	 * Location as required without needing to register a LocationListener.
+	 */
+	public void showMyLocation(View view) {
+		if (mLocationClient != null && mLocationClient.isConnected()) {
+			String msg = "Location = " + mLocationClient.getLastLocation();
+			Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	public void goToTarget(View view) {
+		// Intent intent = getPackageManager().getLaunchIntentForPackage(
+		// "com.mscripts.target");
+		// startActivity(intent);
+
+		final Intent intent = new Intent();
+		final ComponentName cn = new ComponentName(
+				"com.mscripts.android.target",
+				"com.mscripts.target.ActivityLaunchAppLoad");
+		intent.setComponent(cn);
+		intent.setAction(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		// startActivity(intent);
+
+		if (isPackageInstalled("com.mscripts.android.target")) {
+			startActivity(intent);
+		} else {
+			startActivity(new Intent(Intent.ACTION_VIEW,
+					Uri.parse("http://play.google.com/store/apps/details?id="
+							+ "com.mscripts.android.target")));
+		}
+	}
+
+	public boolean isPackageInstalled(String targetPackage) {
+		List<ApplicationInfo> packages;
+		PackageManager pm;
+		pm = getPackageManager();
+		packages = pm.getInstalledApplications(0);
+		for (ApplicationInfo packageInfo : packages) {
+			if (packageInfo.packageName.equals(targetPackage))
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Implementation of {@link LocationListener}.
+	 */
+	@Override
+	public void onLocationChanged(Location location) {
+		mMessageView.setText("Location = " + location);
+	}
+
+	/**
+	 * Callback called when connected to GCore. Implementation of
+	 * {@link ConnectionCallbacks}.
+	 */
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		mLocationClient.requestLocationUpdates(REQUEST, this); // LocationListener
+	}
+
+	/**
+	 * Callback called when disconnected from GCore. Implementation of
+	 * {@link ConnectionCallbacks}.
+	 */
+	@Override
+	public void onDisconnected() {
+		// Do nothing
+		Toast.makeText(getApplicationContext(), "Disconnected",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * Implementation of {@link OnConnectionFailedListener}.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// Do nothing
+	}
+
+	@Override
+	public boolean onMyLocationButtonClick() {
+		Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
+				.show();
+		// Return false so that we don't consume the event and the default
+		// behavior still occurs
+		// (the camera animates to the user's current position).
+		return false;
+	}
+}
